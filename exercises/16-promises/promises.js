2aca7895fe8a67588e488651d9eda1d1065c937e
Noe_G <noe.gonzalezortiz@gmail.com>
2021-08-30 20:51:51 -0400

    "Promises HW- Noe"

--- exercises/16-promises/promises.js
+++ exercises/16-promises/promises.js
@@ -28,7 +28,36 @@
  *
  * @returns {Promise} which will resolve to the JSON above.
  */
-const getBooksApi = () => {};
+const getBooksApi = () => {
+  return new Promise((resolve,reject) =>{
+    window.setTimeout(()=>{
+      //reject();
+      resolve({
+        "category": "books",
+        "subCategory": "graphic novels",
+        "data": [{
+          "id": "0c8cb1b2",
+          "title": "Berlin",
+          "authors": ["Jason Lutes"],
+          "image": "https://images-na.ssl-images-amazon.com/images/I/51DhbDeVIVL._SX388_BO1,204,203,200_.jpg"
+        },{
+          "id": "cb195709",
+          "title": "Hey, Kiddo",
+          "authors": ["Jarrett J. Krosoczka"],
+          "image": "https://images-na.ssl-images-amazon.com/images/I/517I7YRvHBL._SX351_BO1,204,203,200_.jpg"
+        },{
+          "id": "77ae31c1",
+          "title": "On a Sunbeam",
+          "authors": ["Tillie Walden"],
+          "image": "https://images-na.ssl-images-amazon.com/images/I/51Ukxxbo-mL._SX359_BO1,204,203,200_.jpg"
+          }]
+        }
+      );
+    },50);
+  });
+};
+
+
 
 /**
  * @var {array} data an array of graphic novels
@@ -52,7 +81,13 @@ let data; // Do not change this line.
  * @returns something with "getBooksApi()"
  */
 
-const setData = () => {};
+const setData = () => {
+  return getBooksApi()
+  .then((response) => {
+    data = response.data;
+  })
+  .catch(() => console.error('Error!'));
+};
 
 /**
  * Ignore me. This is for the tests
